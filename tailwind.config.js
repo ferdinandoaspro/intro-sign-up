e76ea1668a93928fd2e147a3d0d79224d91a2931
FatChickn <ferdinandoaspro2@gmail.com>
2023-01-29 20:50:40 +0100

    almost completed the mobile design

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,16 @@ module.exports = {
       colors: {
         "red": "hsl(0, 100%, 74%)", 
         "green": "hsl(154, 59%, 51%)",
-        "Blue": "hsl(248, 32%, 49%)",
+        "blue": "hsl(248, 32%, 49%)",
         "dark-blue": "hsl(249, 10%, 26%)", 
         "grayish-blue": "hsl(246, 25%, 77%)"
       },
       fontFamily: {
         "poppins": ["Poppins", "sans-serif"]
+      },
+      backgroundImage: {
+        "mobile": "url('/images/bg-intro-mobile.png')",
+        "desktop": "url('/images/bg-intro-desktop.png')"
       }
     },
   },
