1f80dfdc29047b566f3da66c3a8929703049fc66
FatChickn <ferdinandoaspro2@gmail.com>
2023-01-31 16:21:58 +0100

    introduced basic validation logic

--- script/script.js
+++ script/script.js
@@ -1,9 +1,32 @@
 const inputs = document.querySelectorAll("input");
 
-function Error(e) {
+function returnErrorMessage(e) {
     let name = e.target.getAttribute("name");
 
-    let iconError = document.createElement("span");
-    iconError.setAttribute("data-id", name);
-    iconError.innerHTML = "<img src='images/icon-error.svg' alt='' aria-hidden='true'>";
+    let errorStatus = document.createElement("span");
+    errorStatus.classList.add("flex", "flex-row");
+
+    e.target.after(errorStatus);
+
+    let errorMessage = document.createElement("span");
+    errorMessage.setAttribute("data-id", name);
+    errorMessage.classList.add('text-red', "text-[0.7rem]");
+
+    let errorIcon = document.createElement("span");
+    errorIcon.setAttribute("data-id", name);
+    errorIcon.innerHTML = "<img src='./images/icon-error.svg' alt='' aria-hidden='true'>";
+
+    if (e.target.value === "") {
+        errorMessage.innerText = `${e.target.getAttribute("placeholder")} cannot be empty.`;
+    } else {
+        errorMessage.innerText = `Looks like this is not an ${e.target.getAttribute("placeholder")}.`;
+    }
+
+    errorStatus.appendChild(errorIcon);
+    errorStatus.appendChild(errorMessage);
 }
+
+inputs.forEach(input => {
+    input.addEventListener("blur", () => {checkValidity()});
+    input.addEventListener("invalid", returnErrorMessage)
+})
